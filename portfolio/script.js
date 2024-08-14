672981ccefc44d4463b8dbf2868224111526fcb7
satyampal123-sp <12345satyampal@gmail.com>
2024-08-15 01:46:20 +0530

    Add files via upload

---
+++ portfolio/script.js
@@ -0,0 +1,68 @@
+// circle skill ///////////////////////////////////
+
+const circles = document.querySelectorAll('.circle');
+circles.forEach(elem=>{
+    var dots = elem.getAttribute("data-dots");
+    var marked = elem.getAttribute("data-percent");
+    var percent = Math.floor(dots*marked/100);
+    var points = "";
+    var rotate = 360 / dots;
+
+    for(let i = 0 ; i < dots; i++){
+        points += `<div class="points" style="--i:${i}; --rot:${rotate}deg"></div>`;
+    }
+    elem.innerHTML = points;
+
+    const pointsMarked = elem.querySelectorAll('.points');
+    for(let i = 0; i<percent ; i++){
+        pointsMarked[i].classList.add('marked')
+    }
+})
+
+// Active Menu ///////////////////////////////////
+
+let menuLi = document.querySelectorAll('header ul li a');
+let section = document.querySelectorAll('section');
+
+
+function activeMenu(){
+    let len = section.length;
+    while(--len && window.scrollY + 97 < section[len].offsetTop){}
+    menuLi.forEach(sec => sec.classList.remove("active"));
+    menuLi[len].classList.add("active");
+}
+
+activeMenu();
+window.addEventListener("scroll",activeMenu);
+
+
+
+// Sticky Navbar ///////////////////////////////////
+
+const header  = document.querySelector("header");
+window.addEventListener("scroll" ,function(){
+    header.classList.toggle("sticky",window.scrollY > 50)
+})
+
+
+//toggle icon navbar /////////////////////
+let menuIcon = document.querySelector("#menu-icon");
+let navlist = document.querySelector(".navlist");
+
+menuIcon.onclick = ()=>{
+    menuIcon.classList.toggle("bx-x");
+    navlist.classList.toggle("open");
+}
+
+
+window.onscroll = ()=>{
+    menuIcon.classList.remove("bx-x");
+    navlist.classList.remove("open");
+}
+
+//send message  /////////////////////
+function sendmsg() {
+    alert ('Thank You for connecting with me......')
+        
+   
+}
